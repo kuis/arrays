ec4de5794b1ed91e9778457daae31084186aae8c
Andre Lindquist <andrelindquist@gmail.com>
2017-03-29 15:20:44 -0700

    Track custom attributes for UserEngage

--- public/javascripts/dashboard/controllers/dataset/process.js
+++ public/javascripts/dashboard/controllers/dataset/process.js
@@ -236,10 +236,10 @@ angular.module('arraysApp')
                 if(datasource.sample == true) {
                     $scope.user.sampleImported = true;
                     User.sampleImported( {id:$scope.user._id}, {sampleImported: true} );
-                    window.Intercom('update', {
-                        email: $scope.user.email,
-                        "Sample Viz Created": $scope.user.sampleImported, // Boolean
-                    });
+                    // window.Intercom('update', {
+                    //     email: $scope.user.email,
+                    //     "Sample Viz Created": $scope.user.sampleImported, // Boolean
+                    // });
                 }
 
                 // //Send notification to Intercom when dataset is imported
