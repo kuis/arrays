d1687e23b1a35e80fc5911c44b788e46218634de
Shu Han Ng <nsh1026@uw.edu>
2017-03-30 11:52:36 -0700

    add to default view

--- app/controllers/client/data_preparation/team/show.js
+++ app/controllers/client/data_preparation/team/show.js
@@ -27,10 +27,12 @@ module.exports.BindData = function (req, teamDescription, callback) {
 
         if (dataSourceDescription.connection) {
 
+     
             var default_view = (default_customView) ? default_customView : 'gallery';
-            if (typeof dataSourceDescription.fe_views.default_view !== 'undefined') {
+            if (typeof dataSourceDescription.fe_views.default_view !== 'undefined' && !default_customView) {
                 default_view = dataSourceDescription.fe_views.default_view;
             }
+
  
             var updatedByDisplayName = dataSourceDescription.updatedBy.firstName + ' ' + dataSourceDescription.updatedBy.lastName;
             var authorDisplayName = dataSourceDescription.author.firstName + ' ' + dataSourceDescription.author.lastName;
